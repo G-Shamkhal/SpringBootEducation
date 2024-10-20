069e99fa068ade0b1c055db9c26d5095add8e883
G-Shamkhal <g_shamkhal_2001@mail.ru>
2024-10-20 17:32:50 +0500

    Lab 1 completed

---
+++ lab_1/src/main/java/ru/g_shamkhal/SpringBootEducation/SpringBootEducationApplication.java
@@ -0,0 +1,13 @@
+package ru.g_shamkhal.SpringBootEducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEducationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEducationApplication.class, args);
+    }
+
+}
---
+++ lab_1/src/test/java/ru/g_shamkhal/SpringBootEducation/SpringBootEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.g_shamkhal.SpringBootEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
